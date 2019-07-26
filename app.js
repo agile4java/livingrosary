850790356be7d15847c53f9fdaaa1cc87bad8924
Chad Conway <agile4java@gmail.com>
2019-07-26 15:56:50 -0500

    Use: exphbs:   github:  gnowoel/exphbs   npm:  exphbs   npm install exphbs

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const landingRoute = require("./backend/routes/landing_route");
 
 const app = express();
 
+// Declare Global variables
+app.locals.site = "The Living Rosary";
+
+// Declare variable as view option and available
+// to any view
+app.set('view options', {
+  myvar: 'hello'
+});
+
 // mongoose
 //   .connect(
 //     "mongodb+srv://max:" +
@@ -24,10 +33,14 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/public", express.static(path.join(__dirname, "public")));
-app.use("/static", express.static(path.join(__dirname, 'static')));
-app.use("/mdb", express.static(path.join(__dirname, 'backend/mdb')));
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/static")));
+app.use(express.static(path.join(__dirname, "/mdb")));
+
+
+// Use more than one instance of view engine each with
+// own template cache and partial registry
+app.engine('hbs', exphbs.create());
 app.set('view engine', 'handlebars');
 
 app.use((req, res, next) => {
