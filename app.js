81728815ba3092bbe8b04f568cf096c80d02a105
Chad Conway <agile4java@gmail.com>
2019-07-26 16:36:59 -0500

    Pre:  Commit before branch to try  connect in place of express

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const path = require("path");
 const express = require("express");
 const exphbs = require('express-handlebars');
 const bodyParser = require("body-parser");
+
+// Ref: song940/kelp-static  
+// Branch:  express-static
+const serve = require('express-static');
 // const mongoose = require("mongoose");
 
 const landingRoute = require("./backend/routes/landing_route");
@@ -33,15 +37,44 @@ app.set('view options', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.static(path.join(__dirname, "/static")));
-app.use(express.static(path.join(__dirname, "/mdb")));
+
+// Move static links to individual routes so that
+// Landing page can use 'mdb'
+// API can use 'static'
+// app.use('public', express.static(path.join(__dirname, "/public")));
+// app.use(express.static(path.join(__dirname, "/static")));
+// app.use(express.static(path.join(__dirname, "/mdb")));
+
+// Ref:  song940/kelp-static
+// Branch:  express-static
+// express-static for serving static files
+//app.use(serve(__dirname, '/backend/mdb'));
+//app.use(serve(__dirname, '/static'));
 
 
+
+
+// Ref: ericf/express-handlebars
+var hbs = exphbs.create({
+  partialsDir: [
+    'views/partials/scripts/',
+    'views/partials/nav/',
+    'views/partials/contactinfo/',
+    'views/partials/landingpartials/',
+    'views/partials/'
+  ]
+})
+
+
+// Ref:  exphbs
 // Use more than one instance of view engine each with
 // own template cache and partial registry
-app.engine('hbs', exphbs.create());
-app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+
+// Ref: exphbs
+// Setting view engine will make value the fefault file extension
+// for looking up views
+app.set('view engine', '.handlebars');
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
