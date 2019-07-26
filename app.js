f603bc78c08c7b3507b09868768df268a2b1fbed
Chad Conway <agile4java@gmail.com>
2019-07-26 02:48:31 -0500

    Confusing as all hell

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+let Promise = global.Promise || require('promise');
+
 const path = require("path");
 const express = require("express");
 const exphbs = require('express-handlebars');
 const bodyParser = require("body-parser");
 // const mongoose = require("mongoose");
 
-const landingRoute = require("./backend/routes/landing_route");
+const landingRoutes = require("./backend/routes/landing_routes");
 
 
 const app = express();
@@ -26,8 +28,54 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/static", express.static(path.join(__dirname, 'static')));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+
+// Create 'ExpressHandlebars' instance with a default layout
+const hbs = exphbs.create({
+  helpers: helpers,
+  partialsDir: [
+    'shared/templates/',
+    'views/partials/'
+  ]
+});
+
+// Registering 'hbs' created above as view engine
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', hbs.engine);
+
+// unchanged from regular code
 app.set('view engine', 'handlebars');
+
+// Middleware to expose app's shared templates to the client-side of the app
+
+function exposeTemplates(req, res, next) {
+  // Uses the ExpressHandlebars instance to get the precompiled templates
+  hbs.getTemplates('shared/templates/', {
+    cache: app.enabled('view cache'),
+    precompile: true
+  }).then(function (templates) {
+    // RegExp to remove the ".handlebars" extension from template names
+    var extRegex = new RegExp(habs.extname + '$');
+    // Create an array of templates exposed via
+    // 'res.locals.templates'
+    templates = Object.keys(templates).map(function (name) {
+      return {
+        name: name.replace(extRegex, ''),
+        template: templates[name]
+      };
+    });
+
+    // Expose templates during view rendering
+    if (templates.length) {
+      res.locals.templates = templates;
+    }
+
+    setImmediate(next);
+  })
+    .catch(next);
+}
+
+
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -42,7 +90,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', landingRoute);
+app.use('/', landingRoutes);
+
+app.use('/api', apiRoutes);
 
 // app.get('/', (req, res, next) => {
 //     res.render('home');
--- backend/controllers/landing_controller.js
+++ backend/controllers/landing_controller.js
@@ -1,4 +1,12 @@
 // Route -> host:3000/landing
 exports.getLanding = (req, res, next) => {
-    res.render('landing');
+    res.render('landing', {
+        title: 'Landing',
+        // Override default layout
+        layout: 'landinglayout',
+        // Compile partials
+        partials: Promise.resolve({
+            livingrosarydetails: hbs.handlebars.compile()
+        })
+    });
 }
