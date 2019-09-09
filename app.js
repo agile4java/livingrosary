43bdd5029af3a5ebe44364b78631b3e703e48980
Chad Conway <agile4java@gmail.com>
2019-09-08 22:37:12 -0500

    Progress: Tweaked video, now responsive with correct aspect Worked on cleaning up contact info Implement css in styles partials for each container

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
 require('dotenv').config();
 
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const exphbs = require('express-handlebars');
-const bodyParser = require("body-parser");
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// Ref: song940/kelp-static  
+// Ref: song940/kelp-static
 // Branch:  express-static
 const serve = require('express-static');
 
-
-
 // MODULE IMPORTS
 // const template_controller = require('./backend/controllers/template_controller');
-const landingRoute = require("./backend/routes/landing.route");
-const testingRoute = require("./backend/routes/testing.route");
-
-
+const landingRoute = require('./backend/routes/landing.route');
+const testingRoute = require('./backend/routes/testing.route');
 
 // Declare Global variables
-app.locals.site = "The Living Rosary Devotion";
+app.locals.site = 'The Living Rosary Devotion';
 
 // Declare variable as view option and available
 // to any view
 app.set('view options', {
-  myvar: 'hello'
+	myvar: 'hello'
 });
 
 // const mongoose = require("mongoose");
@@ -52,7 +47,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // API can use 'static'
 // app.use('public', express.static(path.join(__dirname, "/public")));
 // app.use(express.static(path.join(__dirname, "/static")));
-app.use(express.static(path.join(__dirname, "/assets")));
+app.use(express.static(path.join(__dirname, '/assets')));
 
 // Ref:  song940/kelp-static
 // Branch:  express-static
@@ -60,24 +55,24 @@ app.use(express.static(path.join(__dirname, "/assets")));
 // app.use(serve(__dirname, '/assets'));
 //app.use(serve(__dirname, '/static'));
 
-
-
-
 // Ref: ericf/express-handlebars
 var hbs = exphbs.create({
-  viewsDir: 'views/pages',
-  partialsDir: [
-    'views/components/',
-    'views/containers/',
-    'views/layouts/',
-    'views/nav/',
-    'views/pages/',
-    'views/scripts/',
-    'views/testimony_cards/',
-    'views/subcontactcomponents',
-    'shared/templates/'
-  ]
-})
+	viewsDir: 'views/pages',
+	partialsDir: [
+		'views/styles/',
+		'views/components/',
+		'views/containers/',
+		'views/layouts/',
+		'views/nav/',
+		'views/pages/',
+		'views/scripts/',
+		'views/testimony_cards/',
+		'views/subcontactcomponents',
+		'shared/templates/',
+		'views/development',
+		'views/nav/components'
+	]
+});
 
 // Ref:  exphbs
 // Use more than one instance of view engine each with
@@ -93,18 +88,17 @@ app.set('view engine', '.handlebars');
 // Ref: ericf/express-handlebars/blob/master/examples/advanced/server.js (github)
 // precompile templates to send as strings to client-side
 
-
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+	);
+	next();
 });
 
 // landing page route
@@ -120,5 +114,3 @@ app.use('/testing', testingRoute);
 // Ref: https://coderwall.com/p/myzvmg/circular-dependencies-in-node-js
 // export your data before you require anything else in modules where you use module.exports
 module.exports = app;
-
-
